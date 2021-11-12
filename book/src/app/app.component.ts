997f781728073e91308131bdbe37557188e135b2
jexists <jexists486@gmail.com>
2021-11-12 23:43:37 +0900

    setting

---
+++ book/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  searchControl: string;
+
+  @ViewChild('searchForm') searchField: ElementRef;
+}
