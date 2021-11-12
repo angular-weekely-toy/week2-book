be897340e0f70e49fcf30da265ea6b86b164aa59
jexists <jexists486@gmail.com>
2021-11-12 23:41:53 +0900

    backup

--- book/src/environments/environment.ts
+++ book/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  naver_book_client_id: 'vRIFPatmHYXkbRCxZZyZ',
+  naver_book_client_secret : 'X6D5eIfeiy'
 };
 
 /*
